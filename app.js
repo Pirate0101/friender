22b448e7d7b886c9664d83be009993354537e7bc
Pirate0101 <prodipto.dhar@tier5.in>
2022-04-19 16:16:17 +0530

    Base Commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      transports: ['websocket', 'polling'],
+    },
+  });
+
+let interval;
+
+io.on("connection", (socket) => {
+    
+   
+    socket.on('join', (room) => {
+      console.log(`Socket ${socket.id} joining ${room}`);
+      socket.join(room);
+      //let emi="00000tttt";
+      //io.to(room).emit('requestPurchase', emi);
+    });
+
+    
+
+    socket.on('requestUserFacebookEmit', (data) => {
+      console.log("RoomData",data);
+      io.to(data.Room).emit('userFacebookInfoSend', data.userInfo);
+  });
+  socket.on('requestUserFacebookFriendsEmit', (data) => {
+    console.log("RoomData",data);
+    io.to(data.Room).emit('userFacebookFriendSend', data.socketArray);
+});
+});
+
+
+app.use(cors());
+server.listen(port, () => console.log(`Listening on port ${port}`));
